7c8117ca931de6bc0a1e26d7ca0c851712114f49
mfigueireddo <matheusmsfigueiredo@gmail.com>
2025-06-09 16:19:20 -0300

    Arquivos texto personalizados

--- t2/main.c
+++ t2/main.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include <unistd.h> // fork()
-#include <stdlib.h> // exit()
+
+#include <time.h> // time()
+
 #include <sys/wait.h> // wait()
+#include <sys/shm.h> // shmget()
+#include <sys/ipc.h> // IPCs
+#include <sys/stat.h> // S_IUSR, S_IWUSR
 
 #include "utils.h"
 
-#define forProcessos(i) for(int i=0; i<4; i++)
-
 void criaProcessos(void);
 void criaArquivosTexto(void);
+int* geraVetorBaguncado(void);
+char geraReadWrite(void);
+
+int segmento_memoria;
 
 int main(void)
 {
+    // Pega a semente do rand()
+    srand(time(NULL));
+
     // Cria 4 processos
     criaProcessos();
 
@@ -19,6 +31,8 @@ int main(void)
     criaArquivosTexto();
 
     // Cria a memória que será compartilhada pelos processos
+    segmento_memoria = shmget(IPC_PRIVATE, (sizeof(int)*QTD_PAGINAS)*QTD_PROCESSOS, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+    if (segmento_memoria == -1){ fprintf(stderr, "Erro na criação de memória compartilhada.\n"); exit(1); }
 
     // Executa uma thread que será responsável por gerenciar a memória
 
@@ -69,10 +83,17 @@ void criaArquivosTexto(void)
         arquivos[i] = abreArquivoTexto(caminho, 'w');
     }
 
-    // Escreve nos arquivos
+    // Monta os arquivos de cada processo
+    int *nums, modo;
     forProcessos(i)
     {
-        fprintf(arquivos[i], "Teste");
+        nums = geraVetorBaguncado(); // Gera um vetor de números
+
+        forPaginas(j)
+        {
+            modo = geraReadWrite(); // Gera 'W' ou 'R'
+            fprintf(arquivos[i], "%d %c\n", nums[j], modo);
+        }
     }
 
     // Fecha os arquivos
@@ -81,4 +102,29 @@ void criaArquivosTexto(void)
     #if MODO_TESTE
         printf("> Todos os 4 arquivos texto foram criados\n");
     #endif
+}
+
+int* geraVetorBaguncado(void)
+{
+    // Preenche um vetor com números de 0 à 64 (inclusos)
+    int *nums;
+    nums = (int*)malloc(sizeof(int)*64);
+    forPaginas(i){ nums[i] = i+1; }
+
+    int new_pos, temp;
+    for(int old_pos=63; old_pos>0; old_pos--)
+    {
+        new_pos = rand() % (old_pos+1);
+        temp = nums[new_pos];
+        nums[new_pos] = nums[old_pos];
+        nums[old_pos] = temp;
+    }
+
+    return nums;
+}
+
+char geraReadWrite(void)
+{
+    int aleatorio = rand() % 2;
+    return (aleatorio % 2 == 0) ? 'W' : 'R';
 }
--- t2/utils.h
+++ t2/utils.h
@@ -5,6 +5,12 @@
     #define MODO_TESTE 0
     #endif
 
+    #define QTD_PROCESSOS 4
+    #define QTD_PAGINAS 64
+
+    #define forProcessos(i) for(int i=0; i<QTD_PROCESSOS; i++)
+    #define forPaginas(i) for(int i=0; i<QTD_PAGINAS; i++)
+
     // Funções de utils.c
     FILE* abreArquivoTexto(char* caminho, char modo);
     void fechaArquivoTexto(FILE* arquivo);
